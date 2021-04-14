95593e7dc896f7e1c043fa95e89f817a26621fe3
august95 <55133964+august95@users.noreply.github.com>
2021-04-14 14:51:46 +0200

    asdf

--- Source/ProVR/Private/Actions/ProVRRegisterUserAction.cpp
+++ Source/ProVR/Private/Actions/ProVRRegisterUserAction.cpp
@@ -8,10 +8,12 @@
 
 EProVRActionBehavior UProVRRegisterUserAction::PerformAction()
 {
+	
 	TSharedPtr<FJsonObject> RequestJson = MakeShareable(new FJsonObject);
 	RequestJson->SetStringField("emailAddress", EmailAddress);
 	RequestJson->SetStringField("password", Password);
 	RequestJson->SetStringField("username", Username);
+
 
 	UProVRHttpRequest::PostJson(AUTH_SERVICE_REGISTER_REQUEST_PATH, RequestJson, 
 		[this](int32 HttpResponseCode, TSharedPtr<FJsonObject> HttpResponseContent)
@@ -20,25 +22,26 @@ EProVRActionBehavior UProVRRegisterUserAction::PerformAction()
 			{
 				OnActionComplete.Broadcast(EProVRRegisterUserActionResult::ENUM_OK);
 				UE_LOG(LogTemp, Warning, TEXT("register action: on action complete: 200"));
-				UE_LOG(LogTemp, Error, TEXT("%s"), *HttpResponseContent->GetStringField("message"));
 			}
 			else if (HttpResponseCode == HTTP_UNEXPECTED_ERROR || HttpResponseCode >= 500)
 			{
 				OnActionComplete.Broadcast(EProVRRegisterUserActionResult::ENUM_ServerError);
 				UE_LOG(LogTemp, Warning, TEXT("register action: on action complete: 500"));
-				UE_LOG(LogTemp, Error, TEXT("%s"), *HttpResponseContent->GetStringField("message"));
+			}
+			else if (HttpResponseCode == HTTP_UNEXPECTED_ERROR || HttpResponseCode == 406)
+			{
+				OnActionComplete.Broadcast(EProVRRegisterUserActionResult::ENUM_ServerError);
+				UE_LOG(LogTemp, Warning, TEXT("register action: on action complete: 406"));
 			}
 			else if (HttpResponseCode == 409) //Conflict
 			{
 				OnActionComplete.Broadcast(EProVRRegisterUserActionResult::ENUM_UserAlreadyExists);
 				UE_LOG(LogTemp, Warning, TEXT("register action: on action complete: 409"));
-				UE_LOG(LogTemp, Error, TEXT("%s"), *HttpResponseContent->GetStringField("message"));
 			}
 			else if (HttpResponseCode == 400) //Bad Request
 			{
 				OnActionComplete.Broadcast(EProVRRegisterUserActionResult::ENUM_InvalidEmailOrPass);
 				UE_LOG(LogTemp, Warning, TEXT("register action: on action complete: 400"));
-				UE_LOG(LogTemp, Error, TEXT("%s"), *HttpResponseContent->GetStringField("message"));
 			}
 			else
 			{
